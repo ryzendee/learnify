a7f9e618683ebf3df7c308996f6fcea31f6d2c97
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-12-19 13:28:20 +0400

    chore: Add entities for cards & lerrning modules

---
+++ module-service/src/main/java/com/ryzendee/moduleservice/entity/Card.java
@@ -0,0 +1,25 @@
+package com.ryzendee.moduleservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name = "cards")
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String term;
+    private String definition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private LearningModule learningModule;
+}
