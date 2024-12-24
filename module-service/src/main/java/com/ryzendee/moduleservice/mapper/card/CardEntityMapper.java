8c2fd8ebb0601ec39d2772aa9512f97a97184ef4
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-12-24 16:30:47 +0400

    fix: Add mapping for learning module id

--- module-service/src/main/java/com/ryzendee/moduleservice/mapper/card/CardEntityMapper.java
+++ module-service/src/main/java/com/ryzendee/moduleservice/mapper/card/CardEntityMapper.java
@@ -3,11 +3,13 @@ package com.ryzendee.moduleservice.mapper.card;
 import com.ryzendee.moduleservice.dto.card.response.CardResponse;
 import com.ryzendee.moduleservice.entity.CardEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CardEntityMapper {
 
+    @Mapping(source = "learningModuleEntity.id", target = "learningModuleId")
     CardResponse map(CardEntity entity);
 }
