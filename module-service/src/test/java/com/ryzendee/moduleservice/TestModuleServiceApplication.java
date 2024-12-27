fe2fa55cada66a53bd303d579284e25f9723142d
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-12-27 10:22:21 +0500

    test: Move container's components into base abstract class

--- module-service/src/test/java/com/ryzendee/moduleservice/TestModuleServiceApplication.java
+++ module-service/src/test/java/com/ryzendee/moduleservice/TestModuleServiceApplication.java
@@ -1,11 +1,12 @@
 package com.ryzendee.moduleservice;
 
+import com.ryzendee.moduleservice.testutils.base.BaseTestcontainersTest;
 import org.springframework.boot.SpringApplication;
 
 public class TestModuleServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(ModuleServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(ModuleServiceApplication::main).with(BaseTestcontainersTest.class).run(args);
     }
 
 }
--- module-service/src/test/java/com/ryzendee/moduleservice/testutils/base/BaseServiceDbIT.java
+++ module-service/src/test/java/com/ryzendee/moduleservice/testutils/base/BaseServiceDbIT.java
@@ -1,8 +1,8 @@
 package com.ryzendee.moduleservice.testutils.base;
 
-import com.ryzendee.moduleservice.TestcontainersConfiguration;
 import com.ryzendee.moduleservice.testutils.config.TestDatabaseFacadeConfig;
 import com.ryzendee.moduleservice.testutils.facade.TestDatabaseFacade;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,13 +11,18 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@Import({TestDatabaseFacadeConfig.class, TestcontainersConfiguration.class})
+@Import(TestDatabaseFacadeConfig.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-public abstract class BaseServiceDbIT {
+public abstract class BaseServiceDbIT extends BaseTestcontainersTest {
 
     @Autowired
     protected TestDatabaseFacade testDatabaseFacade;
 
     @MockBean
     private SecurityFilterChain securityFilterChain;
+
+    @BeforeAll
+    static void startupPostgresContainer() {
+        postgresContainer.start();
+    }
 }
