37a4d0380f77fdd228a0190683d1b6a6a1c5a792
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2025-01-08 13:38:23 +0500

    chore: Add icons import

--- learnify-ui/src/main.js
+++ learnify-ui/src/main.js
@@ -5,6 +5,7 @@ import { initKeycloak } from './services/keycloak';
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 
+import '@mdi/font/css/materialdesignicons.css';
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
