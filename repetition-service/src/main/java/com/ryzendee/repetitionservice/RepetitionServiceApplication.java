fa6e2900cdccf12ddc72ddcfffb190ecc1918340
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2025-01-16 16:37:53 +0400

    chore: Startup project

---
+++ repetition-service/src/main/java/com/ryzendee/repetitionservice/RepetitionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ryzendee.repetitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepetitionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepetitionServiceApplication.class, args);
+	}
+
+}
