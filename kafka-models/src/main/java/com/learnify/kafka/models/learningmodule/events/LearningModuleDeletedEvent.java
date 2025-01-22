dbb1607bc82d301926e9d4d5779a44561e14a489
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2025-01-22 12:42:36 +0400

    fix: Remove 'final' keyword, add equals & hashcode

--- kafka-models/src/main/java/com/learnify/kafka/models/learningmodule/events/LearningModuleDeletedEvent.java
+++ kafka-models/src/main/java/com/learnify/kafka/models/learningmodule/events/LearningModuleDeletedEvent.java
@@ -1,10 +1,14 @@
 package com.learnify.kafka.models.learningmodule.events;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class LearningModuleDeletedEvent {
 
-    private final UUID learningModuleId;
+    private UUID learningModuleId;
+
+    public LearningModuleDeletedEvent() {
+    }
 
     public LearningModuleDeletedEvent(UUID learningModuleId) {
         this.learningModuleId = learningModuleId;
@@ -14,10 +18,28 @@ public class LearningModuleDeletedEvent {
         return learningModuleId;
     }
 
+    public void setLearningModuleId(UUID learningModuleId) {
+        this.learningModuleId = learningModuleId;
+    }
     @Override
     public String toString() {
         return "LearningModuleDeletedEvent{" +
                 "learningModuleId=" + learningModuleId +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        LearningModuleDeletedEvent that = (LearningModuleDeletedEvent) object;
+
+        return Objects.equals(learningModuleId, that.learningModuleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return learningModuleId != null ? learningModuleId.hashCode() : 0;
     }
 }
